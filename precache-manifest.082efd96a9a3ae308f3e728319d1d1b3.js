4777b0b5d4896a82644e1e68de3b60a4e2bda179
Liming Jin <jin0abc0@uw.edu>
2020-06-13 16:06:59 -0400

    Updates

---
+++ precache-manifest.082efd96a9a3ae308f3e728319d1d1b3.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1039fef5faae1758bd26469e67f9058",
+    "url": "/WolfHouse/index.html"
+  },
+  {
+    "revision": "d0408c1940c05647ed20",
+    "url": "/WolfHouse/static/css/main.8b95d398.chunk.css"
+  },
+  {
+    "revision": "ccab71bea0847ff7dddc",
+    "url": "/WolfHouse/static/js/2.51088183.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/WolfHouse/static/js/2.51088183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0408c1940c05647ed20",
+    "url": "/WolfHouse/static/js/main.efc26e7d.chunk.js"
+  },
+  {
+    "revision": "49343115ef74fb0e566f",
+    "url": "/WolfHouse/static/js/runtime-main.e856ab23.js"
+  },
+  {
+    "revision": "508cdc1d34d7a72fe1288fc06fa312e2",
+    "url": "/WolfHouse/static/media/GuardianModalImage.508cdc1d.jpg"
+  },
+  {
+    "revision": "3bbef829fef3fe90e9d4a6a563a4c0a7",
+    "url": "/WolfHouse/static/media/HunterModalImage.3bbef829.jpg"
+  },
+  {
+    "revision": "9e95913d053d513d18c360b37e2b7c92",
+    "url": "/WolfHouse/static/media/ProphetModalImage.9e95913d.jpg"
+  },
+  {
+    "revision": "316e814f2258c0e6c97aac1ce17a01bc",
+    "url": "/WolfHouse/static/media/WitchModalImage.316e814f.jpg"
+  },
+  {
+    "revision": "82739430dd3181cd9c1dfec93d0fde19",
+    "url": "/WolfHouse/static/media/avatar1.82739430.png"
+  },
+  {
+    "revision": "78cf211ec521ee187c96dd8f6b43ed0f",
+    "url": "/WolfHouse/static/media/avatar10.78cf211e.png"
+  },
+  {
+    "revision": "e671fe6002344dc8a1cec1abe2d8d47b",
+    "url": "/WolfHouse/static/media/avatar11.e671fe60.png"
+  },
+  {
+    "revision": "ea6d4cd12800c5f4841aeacc5ee05d3c",
+    "url": "/WolfHouse/static/media/avatar12.ea6d4cd1.png"
+  },
+  {
+    "revision": "f77728d5f57fae7b80549b3a3469f8fc",
+    "url": "/WolfHouse/static/media/avatar13.f77728d5.png"
+  },
+  {
+    "revision": "cc981ead05090c741800105485a9101d",
+    "url": "/WolfHouse/static/media/avatar14.cc981ead.png"
+  },
+  {
+    "revision": "e8c68cf5b644246bd4902ef9a14c2dea",
+    "url": "/WolfHouse/static/media/avatar15.e8c68cf5.png"
+  },
+  {
+    "revision": "23d850da1ab2c1baa5ba09aa2c6db08f",
+    "url": "/WolfHouse/static/media/avatar2.23d850da.png"
+  },
+  {
+    "revision": "8a356413506e545da7eab7d5d450fd17",
+    "url": "/WolfHouse/static/media/avatar3.8a356413.png"
+  },
+  {
+    "revision": "e75f0d08443dc972e9185223e2a125ef",
+    "url": "/WolfHouse/static/media/avatar4.e75f0d08.png"
+  },
+  {
+    "revision": "a1c9681d09afa1a2e7f5269408f77e44",
+    "url": "/WolfHouse/static/media/avatar5.a1c9681d.png"
+  },
+  {
+    "revision": "bd9d5295a24cecf51fa7c5e90c3657b2",
+    "url": "/WolfHouse/static/media/avatar6.bd9d5295.png"
+  },
+  {
+    "revision": "c67fe8f155ffe857e745475966b2ea48",
+    "url": "/WolfHouse/static/media/avatar7.c67fe8f1.png"
+  },
+  {
+    "revision": "6e75ad9aedbf6770f7b12a59ae5d9d1d",
+    "url": "/WolfHouse/static/media/avatar8.6e75ad9a.png"
+  },
+  {
+    "revision": "3ea695011f7235d4dfe83125ec4ce7c5",
+    "url": "/WolfHouse/static/media/avatar9.3ea69501.png"
+  },
+  {
+    "revision": "b8fb8bbd84b3be294da71dbdbcd55359",
+    "url": "/WolfHouse/static/media/cover.b8fb8bbd.jpg"
+  },
+  {
+    "revision": "9f3b50d3629e13044ca57c5666010d54",
+    "url": "/WolfHouse/static/media/guardian.9f3b50d3.jpg"
+  },
+  {
+    "revision": "a2a558c54d9659c11501287c5a71916a",
+    "url": "/WolfHouse/static/media/hunter.a2a558c5.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WolfHouse/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7296e53e5a676af39e841d5c1ffbcea9",
+    "url": "/WolfHouse/static/media/prophet.7296e53e.jpg"
+  },
+  {
+    "revision": "6622d8262503a6d6698f9349aee187d9",
+    "url": "/WolfHouse/static/media/roomBackground.6622d826.jpg"
+  },
+  {
+    "revision": "e847421aeb953af8eead2b2fcc4e1f0b",
+    "url": "/WolfHouse/static/media/seatAvailable.e847421a.png"
+  },
+  {
+    "revision": "00d9e6f5889041b586892c3382756248",
+    "url": "/WolfHouse/static/media/villager.00d9e6f5.jpg"
+  },
+  {
+    "revision": "eacdc6b1506016e27271a58f7671b217",
+    "url": "/WolfHouse/static/media/witch.eacdc6b1.jpg"
+  },
+  {
+    "revision": "1c495239d03786d1d15ac088b1a2d29b",
+    "url": "/WolfHouse/static/media/wolf.1c495239.jpg"
+  },
+  {
+    "revision": "faaf9227266388d68f65b9514fdd541d",
+    "url": "/WolfHouse/static/media/wolf.faaf9227.mp4"
+  }
+]);
